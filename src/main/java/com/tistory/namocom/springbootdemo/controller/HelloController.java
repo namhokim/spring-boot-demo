e705b6233e6a5aa41a811a411309526d6da952a1
Namho Kim <nhkimnamo@gmail.com>
2018-11-07 16:32:40 +0900

    Test: profile override
    application.yml의 기본 설정중 특정 프로파일이 해당 키의 값만 덮어씌움 검증

--- src/main/java/com/tistory/namocom/springbootdemo/controller/HelloController.java
+++ src/main/java/com/tistory/namocom/springbootdemo/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.tistory.namocom.springbootdemo.controller;
 
 import com.tistory.namocom.springbootdemo.service.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     private final DashboardService dashboardService;
+
+    @Value("${spring.jmx.enabled}")
+    private boolean jmxEnabled;
+
+    @Value("${metric.application}")
+    private String metricApplication;
 
     @Autowired
     public HelloController(DashboardService dashboardService) {
@@ -19,6 +26,7 @@ public class HelloController {
     public String helloWorld() {
         dashboardService.incresePV();
 
-        return "hello world! " + dashboardService.getPV();
+        return String.format("hello world!, page view: %d jmx.enabled: %s, metric.application: %s",
+                dashboardService.getPV(), jmxEnabled, metricApplication);
     }
 }
